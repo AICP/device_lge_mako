dbffda4e2cfd1b238077e39cf4da855949713236
Adam Lesinski <adamlesinski@google.com>
2015-04-23 09:59:26 +0300

    mako: Remove density from PRODUCT_AAPT_CONFIG
    AAPT ignores densities in PRODUCT_AAPT_CONFIG.
    The use of PRODUCT_AAPT_PREF_CONFIG for density is
    encouraged, as AAPT is able to determine the fallback
    density to use if a resource of the specified density
    does not exist.
    
    Change-Id: I428c73de16f3884bf4cc7602acdcd1eabf2084c6

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ DEVICE_PACKAGE_OVERLAYS := device/lge/mako/overlay
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 PRODUCT_PACKAGES := \
